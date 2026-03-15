0887897602639d4adb93c85b7f057f939098a004
Lebedenko Aleksander <lebedenko.aleksander2015@yandex.ru>
2026-03-15 23:13:01 +0300

    Добавлен sql запрос для получения фильмов

--- src/main/java/ru/yandex/practicum/filmorate/dal/LikeFilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/LikeFilmRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.LikeStorage;
 
+import java.time.LocalDate;
 import java.util.Collection;
 
 @Repository
@@ -21,12 +22,27 @@ public class LikeFilmRepository extends BaseRepository<Film> implements LikeStor
             "VALUES (?, ?)";
     private static final String DELETE_QUERY = "DELETE FROM like_film WHERE film_id = ? AND user_id = ?";
 
+    private static final String SORT_FILM_BY_GENRE_YEAR_DESC_LIMIT_QUERY =
+            "SELECT f.id, f.name, f.mpa_id, f.description, f.release_date, f.duration " +
+                    "FROM like_film lf " +
+                    "JOIN film f ON lf.film_id = f.id " +
+                    "JOIN FilmGenre fg ON f.id = fg.film_id " +
+                    "WHERE fg.genre_id = ? " +
+                    "AND f.release_date BETWEEN ? AND ? " +
+                    "GROUP BY f.id, f.name, f.mpa_id, f.description, f.release_date, f.duration " +
+                    "ORDER BY COUNT(lf.user_id) DESC " +
+                    "LIMIT ?";
+
     public LikeFilmRepository(JdbcTemplate jdbc, RowMapper<Film> mapper) {
         super(jdbc, mapper);
     }
 
     public Collection<Film> topFilms(int limit) {
         return jdbc.query(SORT_FILM_DESC_LIMIT_QUERY, mapper, limit);
+    }
+
+    public Collection<Film> popularFilmByYearAndGenre(int genreId, LocalDate yearStart, LocalDate yearEnd, int count) {
+        return jdbc.query(SORT_FILM_BY_GENRE_YEAR_DESC_LIMIT_QUERY, mapper, genreId, yearStart, yearEnd, count);
     }
 
     @Override
